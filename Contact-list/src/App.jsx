2fec005e77c9ed6f0c476f26052d3c90a3504ad4
unknown <adamelmekki3@gmail.com>
2024-07-11 22:51:35 -0500

    finished code

---
+++ Contact-list/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+import ContactList from './components/ContactList';
+import SelectedContact from './components/SelectedContact';
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+  return (
+    <>
+      {selectedContactId ? (
+        <SelectedContact selectedContactId={selectedContactId} setSelectedContactId={setSelectedContactId} />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
+    </>
+  );
+}
---
+++ Contact-list/src/components/ContactList.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import ContactRow from './ContactRow';
+
+export default function ContactList({ setSelectedContactId }) {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch(
+          "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts();
+  }, []);
+
+  console.log("Contacts: ", contacts);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {contacts.map(contact => (
+          <ContactRow key={contact.id} contact={contact} setSelectedContactId={setSelectedContactId} />
+        ))}
+      </tbody>
+    </table>
+  );
+}
